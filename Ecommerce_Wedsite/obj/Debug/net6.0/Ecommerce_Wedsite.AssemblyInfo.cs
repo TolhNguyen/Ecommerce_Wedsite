52fbe58dfd6274b77405b6c3f22662c07f4aa884
TPDongQuan <114727081+TPDongQuan@users.noreply.github.com>
2025-01-03 22:26:02 +0700

    Update(3/1/25)

--- Ecommerce_Wedsite/obj/Debug/net6.0/Ecommerce_Wedsite.AssemblyInfo.cs
+++ Ecommerce_Wedsite/obj/Debug/net6.0/Ecommerce_Wedsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce_Wedsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fddf8698cfa10f7eff536b07ccb084cfd3a355e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86da17b6645a39b004359a9aafe40c3e03f78a05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce_Wedsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce_Wedsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
